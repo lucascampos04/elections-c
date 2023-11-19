1f0ea45d40932620ff568cc4f825ed389b0e7627
lucascampos04 <camposdlucasoli@gmail.com>
2023-11-19 15:58:03 -0300

    Finalizado

--- main.c
+++ main.c
@@ -8,11 +8,10 @@ int main() {
     int escolha, cont1 = 0, cont2 = 0, cont3 = 0;
 
     for (int i = 0; i < 5; ++i) {
-
         printf("Escolha o candidato\n");
-        printf("[1] - C1\n");
-        printf("[2] - C2\n");
-        printf("[3] - C3\n");
+        printf("[1] - João\n");
+        printf("[2] - Maria\n");
+        printf("[3] - Carlos\n");
         scanf("%d", &escolha);
 
         if (escolha == 1) {
@@ -30,28 +29,26 @@ int main() {
         } else if (escolha == 3) {
             if (cont3 < 5) {
                 c3[cont3++] = escolha;
-
             } else {
                 printf("Vetor cheio");
             }
         }
-
     }
 
     if (cont1 > cont2 && cont1 > cont3) {
-        printf("Candidato 1 ganhou com %d de votos\n", cont1);
+        printf("João ganhou com %d de votos\n", cont1);
         for (int i = 0; i < cont1; ++i) {
             printf("%d\n", c1[i]);
         }
         return 0;
     } else if (cont2 > cont1 && cont2 > cont3) {
-        printf("Candidato 2 ganhou com %d de votos\n", cont2);
+        printf("Maria ganhou com %d de votos\n", cont2);
         for (int i = 0; i < cont2; ++i) {
             printf("%d\n", c2[i]);
         }
         return 0;
     } else if (cont3 > cont1 && cont3 > cont2) {
-        printf("Candidato 3 ganhou com %d de votos\n", cont3);
+        printf("Carlos ganhou com %d de votos\n", cont3);
         for (int i = 0; i < cont3; ++i) {
             printf("%d\n", c3[i]);
         }
